e123693eef0fc30ed8c5ac9325a84bb2542e49f8
naoki kodama <naoki.koda@gmail.com>
2011-09-28 23:12:22 +0900

    initial commit 固定でお好みの twitter id のATND情報を表示できるようにした 今後は任意の twitter id を アプリ上から設定できるようにする

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    @twitter_ids = [
+      "snoozer05",
+      "tricknotes",
+      "irasally",
+      "hachiilcane",
+    ]
+
+  end
+
+  def show
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MioAtnd::Application.config.secret_token = '273f1a5edc603312a90508520d11270cdc6258f28273318101b698b354a5cafc4dcad7cc30902a3661606332b317c7e36b1a2ceb825f4db1205083d195b032dd'
