3d8e2cdbddc9d1f885aeb2408e584528e0014e70
naoki kodama <naoki.koda@gmail.com>
2011-10-12 23:53:56 +0900

    他ユーザの一覧を見えなくした。 ログイン後は自分の画面へ飛ぶ。 ログイン前はログイン画面を表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    if current_user 
+      redirect_to current_user
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
